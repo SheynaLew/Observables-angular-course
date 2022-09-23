1446db10bb0d113d9b81c90d36f53d2403eb8cc2
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-09-23 16:41:47 +0100

    178 subjects - everything saved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscriber, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
 
 @Component({
