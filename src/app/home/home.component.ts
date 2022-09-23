a5b27dc096b59873e504512be25d1b42d1cd564e
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-09-23 15:09:36 +0100

    174 building a custom observable - done almost entirely without video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { count } from 'console';
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +9,34 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private countingObservableSubscription: Subscription
+  private countingObservableSubscription: Subscription;
+  private customCountingObservableSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+
+    const customCountingObservable = new Observable<number>((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count)
+        count++;
+      }, 1500);
+    });
+
     this.countingObservableSubscription = interval(1000).subscribe(count => {
       console.log(count)
+    })
+
+    this.customCountingObservableSubscription = customCountingObservable.subscribe(data => {
+      console.log(data);
     })
   }
 
   ngOnDestroy() {
     this.countingObservableSubscription.unsubscribe();
+    this.customCountingObservableSubscription.unsubscribe();
+    // Remember to unsubscribe from anything not provided by Angular!
   }
 
 }
