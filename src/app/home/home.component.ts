24bc8085c44c33468e68914058b0e2a767ac7491
Jenny Lewis-Gandy <jennylewis-gandy@ip-192-168-1-125.eu-west-1.compute.internal>
2022-09-23 14:24:08 +0100

    created a counting subscribtion and stopped it on destroy. Mostly done without video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { count } from 'console';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private countingObservableSubscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
+    this.countingObservableSubscription = interval(1000).subscribe(count => {
+      console.log(count)
+    })
+  }
+
+  ngOnDestroy() {
+    this.countingObservableSubscription.unsubscribe();
   }
 
 }
